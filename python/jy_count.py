8741fda53c88a40be1da8978aa79ebaa39d526f4
PParkJy <wldus8677@gmail.com>
2020-04-27 22:52:22 +0900

    docs: create new directory

---
+++ python/jy_count.py
@@ -0,0 +1,34 @@
+'''
+I found out the count method while processing the string.
+How to use the count method?
+
+Version: python 3.7
+Author: Jiyeon Park (wldus8677@gmail.com)
+
+Syntax>
+1) str.count(sub[, start[, end]])
+2) list.count(value)
+3) bytes.count(value)
+4) bytearray.count(value)
+5) array.array.count(value)
+6) collections.deque.count(value)
+7) itertools.count(value)
+'''
+
+# string
+ex = "Hello everyone"
+print(ex.count('e')) # print the number of times the value 'e' appears in the ex string
+print(ex.count('e'[, 1[,7]]) 
+
+#list
+
+
+
+
+'''
+reference>
+1) https://zetawiki.com/wiki/%ED%8C%8C%EC%9D%B4%EC%8D%AC_%EB%AC%B8%EC%9E%90%EC%97%B4_count()_%EB%A9%94%EC%86%8C%EB%93%9C
+2) https://www.w3schools.com/python/ref_list_count.asp
+3) https://docs.python.org/3.7/library/
+'''
+
